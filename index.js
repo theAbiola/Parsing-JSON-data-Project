e5e969f73af0da0aa505a8b2b32e7a3b830d26f8
Abdulraheem Habib Abiola <iamhabibabiola@gmail.com>
2025-02-03 10:41:05 +0100

    Revamp codebase to use the fashion.json file and add new CSS styling

--- index.js
+++ index.js
@@ -1,46 +1,47 @@
 import express from "express";
 import bodyParser from "body-parser";
+import env from "dotenv";
 
 const app = express();
-const port = 3000;
+env.config();
+const port = process.env.PORT;
 
-const recipeJSON =
-  '[{"id": "0001","type": "taco","name": "Chicken Taco","price": 2.99,"ingredients": {"protein": {"name": "Chicken","preparation": "Grilled"},  "salsa": {"name": "Tomato Salsa","spiciness": "Medium"},  "toppings": [{"name": "Lettuce",  "quantity": "1 cup",  "ingredients": ["Iceberg Lettuce"]  },      {"name": "Cheese",  "quantity": "1/2 cup",  "ingredients": ["Cheddar Cheese", "Monterey Jack Cheese"]  },      {"name": "Guacamole",  "quantity": "2 tablespoons",  "ingredients": ["Avocado", "Lime Juice", "Salt", "Onion", "Cilantro"]  },      {"name": "Sour Cream",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream"]  }      ]    }  },{"id": "0002","type": "taco","name": "Beef Taco","price": 3.49,"ingredients": {"protein": {"name": "Beef","preparation": "Seasoned and Grilled"},  "salsa": {"name": "Salsa Verde","spiciness": "Hot"},  "toppings": [{"name": "Onions",  "quantity": "1/4 cup",  "ingredients": ["White Onion", "Red Onion"]  },      {"name": "Cilantro",  "quantity": "2 tablespoons",  "ingredients": ["Fresh Cilantro"]  },      {"name": "Queso Fresco",  "quantity": "1/4 cup",  "ingredients": ["Queso Fresco"]  }      ]    }  },{"id": "0003","type": "taco","name": "Fish Taco","price": 4.99,"ingredients": {"protein": {"name": "Fish","preparation": "Battered and Fried"},  "salsa": {"name": "Chipotle Mayo","spiciness": "Mild"},  "toppings": [{"name": "Cabbage Slaw",  "quantity": "1 cup",  "ingredients": [    "Shredded Cabbage",    "Carrot",    "Mayonnaise",    "Lime Juice",    "Salt"          ]  },      {"name": "Pico de Gallo",  "quantity": "1/2 cup",  "ingredients": ["Tomato", "Onion", "Cilantro", "Lime Juice", "Salt"]  },      {"name": "Lime Crema",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream", "Lime Juice", "Salt"]  }      ]    }  }]';
+const fashionJSON = process.env.fashionJSON;
 
-const recipe = JSON.parse(recipeJSON);
+const fashion = JSON.parse(fashionJSON);
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let menuOptions = ""
+let outfitOptions = ""
 
 app.get("/", (req, res) => {
-  res.render("index.ejs"); //{menuOptions: menuOptions}
+  res.render("index.ejs");
 });
 
-app.post("/recipe", (req, res) => {
-  let taco = "";
+app.post("/fashion", (req, res) => {
+  let outfit = "";
 
   let choice = req.body.choice;
   switch (choice) {
-    case "chicken":
-     taco = recipe[0];
-   break;
+    case "trad":
+      outfit = fashion[0];
+      break;
 
-   case "beef":
-    taco = recipe[1];
-  break;
+    case "casual":
+      outfit = fashion[1];
+      break;
 
-  case "fish":
-    taco = recipe[2];
-  break;
+    case "corporate":
+      outfit = fashion[2];
+      break;
 
-  default:
-    res.send("The input is invalid");
-    console.log("The provided input is invalid");
-  break;
+    default:
+      res.send("The input is invalid");
+      console.log("The provided input is invalid");
+      break;
   }
 
-  res.render("index.ejs", {menuOption: taco})
+  res.render("index.ejs", { outfitOption: outfit })
 });
 
 app.listen(port, () => {
